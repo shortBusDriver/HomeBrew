e903a2e41bb590631a3bbbdc4ec349c3c1218d6b
Joshua Monti <joshuamonti@Joshuas-MacBook-Pro.local>
2012-10-01 00:16:33 -0400

    New code

---
+++ script.js
@@ -0,0 +1,17 @@
+		
+
+
+
+   function estUse(watt){
+	   var cost =  watt * 24 * 30.5; // watts * 24 hours * 730 hours ( 30.5 days) in a month.
+	   var est = cost / 1000;        // KwH conversion
+	   var total = est * .11361;     //KWh *s SCEG .11261
+	   
+	  return total;
+	   
+   };		
+
+
+
+alert(estUse(200))
+
